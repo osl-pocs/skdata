37974b4d1e198978c8bbf6531507f00e351c7c10
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2017-09-11 23:05:44 -0400

    Changing skdata internal structure to hdf5

--- skdata/data.py
+++ skdata/data.py
@@ -1,6 +1,10 @@
+from datetime import datetime
 # local from import
+
 from . import cleaning
 
+import h5py
+import os
 import pandas as pd
 import numpy as np
 
@@ -15,32 +19,17 @@ class SkData:
     data = None
     categories = {}
     target = None
+    path = ''
 
     def __call__(self, *args, **kwargs) -> pd.DataFrame:
         return self.data
 
-    def __init__(
-        self, data: pd.DataFrame = None, file_path: str = None,
-        index_col: str=None, target_col: str=None
-    ):
-        """
+    def __del__(self):
+        self.data.flush()
+        self.data.close()
 
-        :param data:
-        :param file_path: accepts sources as CSV, ...
-        :param index_col:
-        :param target_col:
-        """
-        if data is None:
-            if file_path is None:
-                raise Exception('[EE] Please inform data object or filename.')
-
-            data = pd.read_csv(file_path, index_col=index_col)
-
-        self._data = data.copy()
-        self.data = data.copy()
-
-        if target_col is not None:
-            self.target = self.data[target_col]
+    def __init__(self, file_path: str):
+        self.load(file_path)
 
     def categorize(
         self, col_name: str=None, categories: dict = None,
@@ -87,12 +76,121 @@ class SkData:
             )
             # TODO: Add log information
 
+    def import_from(
+        self, source: str, dset_id: str=None,
+        index_col: str=None, target_col: str=None
+    ):
+        """
+
+        :param source: accepts sources as CSV, ...
+        :param dset_id: data set id
+        :param index_col:
+        :param target_col:
+        """
+        read_from = {
+            'csv': pd.read_csv
+        }
+
+        try:
+            _source = source.split('.')
+            ext = _source[-1].lower()
+            file_name = _source[-2].lower().split(os.path.sep)[-1]
+        except Exception:
+            raise Exception('[EE] Please inform a filename with extension.')
+
+        data = read_from[ext](source)
+
+        if dset_id is None:
+            dset_id = file_name
+
+        dtypes = np.dtype([
+            (k, pandas_dtype_to_hdf5(data[k]))
+            for k in list(data.keys())
+        ])
+
+        dset = self.data.create_dataset(
+            dset_id, shape=(data.shape[0],),
+            dtype=dtypes
+        )
+
+        for k in data.keys():
+            dset[k] = data[k]
+
+        if target_col is not None:
+            dset.attrs['target'] = target_col
+
+        if index_col is not None:
+            dset.attrs['index'] = index_col
+
+        self.log(dset_id=dset_id, message='Data set creation.')
+
+        self.data.flush()
+
+    def get_data(self, dset_id: str):
+        dset = self.data[dset_id]
+
+        index_col = dset.attrs['index']
+
+        keys = tuple(
+            k for k in dset.dtype.names[:]
+            if k not in [index_col]
+        )
+
+        return pd.DataFrame(
+            dset[keys], index=dset[index_col]
+        )
+
+    def load(self, file_path: str):
+        self.path = file_path
+        self.data = h5py.File(file_path, 'w')
+
+    def log(self, dset_id: str, message: str):
+        """
+
+        :param dset_id:
+        :param message:
+        :return:
+
+        """
+        dset_log_id = '_%s_log' % dset_id
+
+        if dset_log_id not in self.data.keys():
+            dset = self.data.create_dataset(
+                dset_log_id, shape=(1,),
+                dtype=np.dtype([
+                    ('dt_log', '<i8'),
+                    ('message', 'S250')
+                ])
+            )
+        else:
+            dset = self.data[dset_id]
+
+        timestamp = np.array(
+            datetime.now().strftime("%s")
+        ).astype('<i8').view('<M8[s]')
+
+        dset['dt_log'] = timestamp.view('<i8')
+        dset['message'] = message
+        self.data.flush()
+
     def summary(self) -> pd.DataFrame:
         """
 
         :return:
         """
         return summary(self.data)
+
+
+def pandas_dtype_to_hdf5(d):
+    """
+
+    :param d:
+    :return:
+    """
+    return (
+        d.dtype if not d.dtype == pd.api.types.pandas_dtype('O') else
+        'S%s' % int(d.str.len().max())
+    )
 
 
 def summary(data: pd.DataFrame) -> pd.DataFrame:
